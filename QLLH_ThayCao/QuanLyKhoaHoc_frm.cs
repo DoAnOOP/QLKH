b8e0c92ec7279f6af8848690a7d31d14297b70e9
DoAnOOP <phongnguyenhoai2003@gmail.com>
2023-05-11 16:04:57 +0700

    Xử lý back-end cho Giao diện
    Đổi text thành num

--- QLLH_ThayCao/QuanLyKhoaHoc_frm.cs
+++ QLLH_ThayCao/QuanLyKhoaHoc_frm.cs
@@ -13,7 +13,7 @@ namespace QLLH_ThayCao
 {
     public partial class QuanLyKhoaHoc_frm : Form
     {
-        
+
         DataClasses1DataContext dc1 = new DataClasses1DataContext();
         public QuanLyKhoaHoc_frm()
         {
@@ -29,36 +29,37 @@ namespace QLLH_ThayCao
             // DataClasses1DataContext dc1 = new DataClasses1DataContext();
             var dskh = dc1.tbl_KhoaHocs;
             DanhSachKhoaHoc_dgv.DataSource = dskh;
-        }     
+        }
         private void Them_but_Click(object sender, EventArgs e)
         {
-            try { 
+            try
+            {
 
-            // Xu ly Su Kien click nut Them
-            // lấy dữ liệu từ các textbox
-            /* string MaKhoaHoc = MaKhoaHoc_txt.Text;*/
-            tbl_KhoaHoc newObj = new tbl_KhoaHoc();
-            newObj.MaKhoaHoc = MaKhoaHoc_txt.Text;
-            newObj.TenKhoaHoc = TenKhoaHoc_txt.Text;
-            newObj.ThoiGian = ThoiGian_txt.Text;
-            newObj.GioiHanSinhVien = int.Parse(GioiHanGiangVien_txt.Text);
-            newObj.GioiHanGiangVien = Convert.ToInt32(GioiHanGiangVien_txt.Text);
-            newObj.KinhPhiDongGop = Convert.ToInt32(KinhPhi_txt.Text);
-            newObj.SoBuoiLyThuyet = Convert.ToInt32(SoBuoiLyThuyet_txt.Text);
-            newObj.SoBuoiThucHanh = Convert.ToInt32(SoBuoiThucHanh_txt.Text);
-            dc1.tbl_KhoaHocs.InsertOnSubmit(newObj);
-            dc1.SubmitChanges();              
-            MessageBox.Show("Them Khoa Hoc Thanh Cong!");
+                // Xu ly Su Kien click nut Them
+                // lấy dữ liệu từ các textbox
+                /* string MaKhoaHoc = MaKhoaHoc_txt.Text;*/
+                tbl_KhoaHoc newObj = new tbl_KhoaHoc();
+                newObj.MaKhoaHoc = MaKhoaHoc_txt.Text;
+                newObj.TenKhoaHoc = TenKhoaHoc_txt.Text;
+                newObj.ThoiGian = ThoiGian_txt.Text;
+                newObj.GioiHanSinhVien = Convert.ToInt32(GioiHanSinhVien_num.Value);
+                newObj.GioiHanGiangVien = Convert.ToInt32(GioiHanGiangVien_num.Value);
+                newObj.KinhPhiDongGop = Convert.ToInt32(KinhPhi_num.Value);
+                newObj.SoBuoiLyThuyet = Convert.ToInt32(SoBuoiLyThuyet_num.Value);
+                newObj.SoBuoiThucHanh = Convert.ToInt32(SoBuoiThucHanh_num.Value);
+                dc1.tbl_KhoaHocs.InsertOnSubmit(newObj);
+                dc1.SubmitChanges();
+                MessageBox.Show("Them Khoa Hoc Thanh Cong!");
                 /*        newObj = new tbl_KhoaHoc();
                         BindingSource source = new BindingSource();
                         source.DataSource = newObj;
                         DanhSachKhoaHoc_dgv.DataSource = source;
                          Cập nhật dữ liệu  
                         source.ResetBindings(false);*/
-            loaddata_4_DSKH();            
+                loaddata_4_DSKH();
             }
             catch (Exception ex)
-                {
+            {
                 MessageBox.Show("Them Khoa Hoc Khong Thanh Cong");
             }
 
@@ -66,23 +67,23 @@ namespace QLLH_ThayCao
 
         private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-            try { 
-            DataGridViewRow row = new DataGridViewRow();
-            row = DanhSachKhoaHoc_dgv.Rows[e.RowIndex];
-            MaKhoaHoc_txt.Text = row.Cells["MaKhoaHoc"].Value.ToString();
-            MaKhoaHoc_txt.ReadOnly = true;
-            TenKhoaHoc_txt.Text = row.Cells["TenKhoaHoc"].Value.ToString();
-            ThoiGian_txt.Text = Convert.ToString(row.Cells["ThoiGian"].Value);
-            GioiHanSinhVien_txt.Text =Convert.ToString(row.Cells["GioiHanSinhVien"].Value);
-            GioiHanGiangVien_txt.Text = Convert.ToString(row.Cells["GioiHanGiangVien"].Value); ;
-            // nếu kinh phí = NULL => lỗi.
-            KinhPhi_txt.Text = Convert.ToString(row.Cells["KinhPhiDongGop"].Value);
-            SoBuoiLyThuyet_txt.Text = Convert.ToString(row.Cells["SoBuoiLyThuyet"].Value);
-            SoBuoiThucHanh_txt.Text = Convert.ToString(row.Cells["SoBuoiThucHanh"].Value);
+            try
+            {
+                DataGridViewRow row = new DataGridViewRow();
+                row = DanhSachKhoaHoc_dgv.Rows[e.RowIndex];
+                MaKhoaHoc_txt.Text = row.Cells["MaKhoaHoc"].Value.ToString();
+                MaKhoaHoc_txt.ReadOnly = true;
+                TenKhoaHoc_txt.Text = row.Cells["TenKhoaHoc"].Value.ToString();
+                ThoiGian_txt.Text = Convert.ToString(row.Cells["ThoiGian"].Value);
+                GioiHanSinhVien_num.Text = Convert.ToString(row.Cells["GioiHanSinhVien"].Value);
+                GioiHanGiangVien_num.Text = Convert.ToString(row.Cells["GioiHanGiangVien"].Value); ;
+                // nếu kinh phí = NULL => lỗi.
+                KinhPhi_num.Text = Convert.ToString(row.Cells["KinhPhiDongGop"].Value);
+                SoBuoiLyThuyet_num.Text = Convert.ToString(row.Cells["SoBuoiLyThuyet"].Value);
+                SoBuoiThucHanh_num.Text = Convert.ToString(row.Cells["SoBuoiThucHanh"].Value);
             }
-            catch(Exception ex) 
-            { if (KinhPhi_txt.Text == null)
-                    KinhPhi_txt.Text = "0";
+            catch (Exception ex)
+            {
                 throw ex;
             }
         }
@@ -96,11 +97,11 @@ namespace QLLH_ThayCao
             editObj.TenKhoaHoc = TenKhoaHoc_txt.Text;
             editObj.TenKhoaHoc = TenKhoaHoc_txt.Text;
             editObj.ThoiGian = ThoiGian_txt.Text;
-            editObj.GioiHanSinhVien = int.Parse(GioiHanGiangVien_txt.Text);
-            editObj.GioiHanGiangVien = Convert.ToInt32(GioiHanGiangVien_txt.Text);
-            editObj.KinhPhiDongGop = Convert.ToInt32(KinhPhi_txt.Text);
-            editObj.SoBuoiLyThuyet = Convert.ToInt32(SoBuoiLyThuyet_txt.Text);
-            editObj.SoBuoiThucHanh = Convert.ToInt32(SoBuoiThucHanh_txt.Text);
+            editObj.GioiHanSinhVien = Convert.ToInt32(GioiHanSinhVien_num.Value);
+            editObj.GioiHanGiangVien = Convert.ToInt32(GioiHanGiangVien_num.Value);
+            editObj.KinhPhiDongGop = Convert.ToInt32(KinhPhi_num.Value);
+            editObj.SoBuoiLyThuyet = Convert.ToInt32(SoBuoiLyThuyet_num.Value);
+            editObj.SoBuoiThucHanh = Convert.ToInt32(SoBuoiThucHanh_num.Value);
             dc1.SubmitChanges();
             MaKhoaHoc_txt.ReadOnly = false;
             MessageBox.Show("Đã Sửa Thành Công!");
@@ -113,12 +114,14 @@ namespace QLLH_ThayCao
             MaKhoaHoc_txt.Text = "";
             TenKhoaHoc_txt.Text = "";
             ThoiGian_txt.Text = "";
-            GioiHanSinhVien_txt.Text = "";
-            GioiHanGiangVien_txt.Text = "";
-            KinhPhi_txt.Text = "";
-            SoBuoiLyThuyet_txt.Text = "";
-            SoBuoiThucHanh_txt.Text = "";
+            GioiHanSinhVien_num.Text = "";
+            GioiHanGiangVien_num.Text = "";
+            KinhPhi_num.Text = "";
+            SoBuoiLyThuyet_num.Text = "";
+            SoBuoiThucHanh_num.Text = "";
         }
+
+
+
     }
-    
 }
